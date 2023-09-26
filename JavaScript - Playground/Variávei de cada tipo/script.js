f3c19f40267eaf2b7b9607863a448b300bc0ee55
Rui_Passos <119951441+Ziben78@users.noreply.github.com>
2023-09-26 17:56:39 +0100

    Add files via upload
    Eu ainda tenho algumas dificuldades com o github, portanto fui pela via mais arriscada. Fiz os projectos e só depois é que criei o repositório

---
+++ JavaScript - Playground/Variávei de cada tipo/script.js
@@ -0,0 +1,13 @@
+// Variável de tipo String: 
+var meuNome = "Rui Passos";
+
+// Variável do tipo Number
+var meuNumero = 1066;
+
+// Variável do tipo Boolean
+var isDeveloper = false;
+
+// Imprimindo as variáveis no console
+console.log("String: " + meuNome);
+console.log("Number: " + meuNumero);
+console.log("Boolean: " + isDeveloper);
