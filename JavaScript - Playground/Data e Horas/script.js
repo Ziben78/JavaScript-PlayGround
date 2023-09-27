f92421339abfd732666a28ae81dc74fa64bead50
Rui_Passos <119951441+Ziben78@users.noreply.github.com>
2023-09-27 10:45:14 +0100

    Delete JavaScript - Playground directory
    pasta antiga com ficheiros não revistos

--- JavaScript - Playground/Data e Horas/script.js
+++
@@ -1,8 +0,0 @@
-// Função para obter a data e hora atual
-function mostrarDataHora() {
-    var elementoDataHora = document.getElementById("data-hora");
-    var dataHoraAtual = new Date();
-    var dataHoraFormatada = dataHoraAtual.toLocaleString(); // Formata a data e hora
-
-    elementoDataHora.textContent = "Data e Hora Atuais: " + dataHoraFormatada;
-}
